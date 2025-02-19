6369ac09930657dd93b682f8c1a4fbb5832de3c5
Katarina Sandelić <skatesandelic@gmail.com>
2025-02-19 09:19:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function opseg(){
+    let a = document.getElementById("prviBroj").value;
+    let b = document.getElementById("drugiBroj").value;
+
+    let opseg = (2 * a) + (2 * b);
+
+    document.getElementById("opseg").innerHTML= opseg;
+
+}
+function povrsina(){
+    let a = document.getElementById("prviBroj").value;
+    let b = document.getElementById("drugiBroj").value;
+
+    let povrsina = a * b;
+
+    document.getElementById("povrsina").innerHTML= povrsina;
+
+}
